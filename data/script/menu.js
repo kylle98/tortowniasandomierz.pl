dc9fd9fb69f3a166658e55b3cba5c87f0a6db7c1
kartovel <49073563+kartovel@users.noreply.github.com>
2021-03-15 20:47:18 +0100

    Create subsite, finished contact and footer, debug with W3C

--- data/script/menu.js
+++ data/script/menu.js
@@ -1,34 +1,26 @@
     isClose = true;
-    function menusetting(action) 
+    function menusetting(action)
     {
-        if ( action == 1 ) 
-        {
-            for ( let i = 0; i < document.getElementsByClassName("mobile-button").length; i++ ) 
-            {
+        if (action === 1) {
+            for (let i = 0; i < document.getElementsByClassName("mobile-button").length; i++) {
                 document.getElementsByClassName("mobile-button")[i].style.display = "none";
             }
-            document.getElementsByClassName("mobile-oppacity")[0].style.display = "none";
+            document.getElementsByClassName("mobile-opacity")[0].style.display = "none";
             document.getElementsByClassName("mobile")[0].style.display = "none";
             isClose = true;
-        } 
-        else
-        {
-            if ( isClose )
-            {
-                for ( let i = 0; i < document.getElementsByClassName("mobile-button").length; i++ ) {
+        } else {
+            if (isClose) {
+                for (let i = 0; i < document.getElementsByClassName("mobile-button").length; i++) {
                     document.getElementsByClassName("mobile-button")[i].style.display = "block";
                 }
-                document.getElementsByClassName("mobile-oppacity")[0].style.display = "block";
+                document.getElementsByClassName("mobile-opacity")[0].style.display = "block";
                 document.getElementsByClassName("mobile")[0].style.display = "block";
                 isClose = false;
-            }
-            else 
-            {
-                for ( let i = 0; i < document.getElementsByClassName("mobile-button").length; i++ ) 
-                {
+            } else {
+                for (let i = 0; i < document.getElementsByClassName("mobile-button").length; i++) {
                     document.getElementsByClassName("mobile-button")[i].style.display = "none";
                 }
-                document.getElementsByClassName("mobile-oppacity")[0].style.display = "none";
+                document.getElementsByClassName("mobile-opacity")[0].style.display = "none";
                 document.getElementsByClassName("mobile")[0].style.display = "none";
                 isClose = true;
             }
