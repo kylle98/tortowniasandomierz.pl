9425ceaebf9739cdc9e2c9ff42cac681a6baefa0
kartovel <49073563+kartovel@users.noreply.github.com>
2022-06-22 22:07:54 +0200

    Create gallery sorting site's
    Created gallery sorting site's
    Fixed gallery.js

--- data/script/gallery.js
+++ data/script/gallery.js
@@ -19,5 +19,4 @@ function showSlides(n) {
     }
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
 }
